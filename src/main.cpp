ee2b13bf396e553c43c7f8e1eef53bfe3bdeafe7
AnastaiaLos <anastasiy.los@yandex.ru>
2026-03-29 22:32:22 +0300

    3main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 // Copyright 2022 UNN-IASR
 #include <iostream>
 #include "fun.h"
-
 int main() {
     const char* str1 = "aaaaabbbb 1369  dfv";
     const char* str2 = "Sj Ddfv";
@@ -9,7 +8,6 @@ int main() {
     const char* str4 = "CJkdl dsk123 evcss";
     const char* str5 = "d dccddc dcc Dcccc";
     const char* str6 = "dfgh sdfg sdf zxc";
-
     std::cout << "First fun" << std::endl;
     std::cout << faStr1(str1) << std::endl;
     std::cout << faStr1(str2) << std::endl;
@@ -17,7 +15,6 @@ int main() {
     std::cout << faStr1(str4) << std::endl;
     std::cout << faStr1(str5) << std::endl;
     std::cout << faStr1(str6) << std::endl;
-
     std::cout << "Second fun" << std::endl;
     std::cout << faStr2(str1) << std::endl;
     std::cout << faStr2(str2) << std::endl;
@@ -25,7 +22,6 @@ int main() {
     std::cout << faStr2(str4) << std::endl;
     std::cout << faStr2(str5) << std::endl;
     std::cout << faStr2(str6) << std::endl;
-
     std::cout << "Third fun" << std::endl;
     std::cout << faStr3(str1) << std::endl;
     std::cout << faStr3(str2) << std::endl;
@@ -33,6 +29,5 @@ int main() {
     std::cout << faStr3(str4) << std::endl;
     std::cout << faStr3(str5) << std::endl;
     std::cout << faStr3(str6) << std::endl;
-
     return 0;
 }
