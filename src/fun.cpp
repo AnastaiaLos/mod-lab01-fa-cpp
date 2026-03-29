1d82aec942f6dbf3998dc668d3316ba885f5f6eb
AnastaiaLos <anastasiy.los@yandex.ru>
2026-03-29 22:19:47 +0300

    2 fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -5,7 +5,6 @@ unsigned int faStr1(const char *str) {
         unsigned int quantity = 0;
     bool meaning = false;
     bool item = false;
-
     for (int j = 0; str[j] != '\0'; j++) {
         if (!isspace(str[j])) {
             if (!meaning) {
@@ -15,50 +14,40 @@ unsigned int faStr1(const char *str) {
             if (isdigit(str[j])) {
                 item = true;
             }
-        }
-        else {
+        } else {
             if (meaning) {
                 if (!item) quantity++;
                 meaning = false;
             }
         }
     }
-
     if (meaning && !item) quantity++;
-
     return quantity;
 }
-
 unsigned int faStr2(const char* str) {
     unsigned int quantity = 0;
     bool meaning = false;
     bool good = true;
-
     for (int k = 0; str[k] != '\0'; k++) {
         if (!isspace(str[k])) {
             if (!meaning) {
                 meaning = true;
                 good = isupper(str[k]); 
-            }
-            else {
+            } else {
                 if (!islower(str[k])) {
                     good = false;
                 }
             }
-        }
-        else {
+        } else {
             if (meaning) {
                 if (good) quantity++;
                 meaning = false;
             }
         }
     }
-
     if (meaning && good) quantity++;
-
     return quantity;
 }
-
 unsigned int faStr3(const char* str) {
     unsigned int allSUM = 0;
     unsigned int words = 0;
@@ -67,8 +56,7 @@ unsigned int faStr3(const char* str) {
     for (int i = 0; str[i] != '\0'; i++) {
         if (!isspace(str[i])) {
             currentLen++;
-        }
-        else {
+        } else {
             if (currentLen > 0) {
                 allSUM += currentLen;
                 words++;
@@ -76,15 +64,11 @@ unsigned int faStr3(const char* str) {
             }
         }
     }
-
     if (currentLen > 0) {
         allSUM += currentLen;
         words++;
     }
-
     if (words == 0) return 0;
-
-    double avg = (double)allSUM / words;
-
-    return (unsigned int)(avg + 0.5);
+    double avg = static_cast<double>(allSUM) / words;
+    return static_cast<unsigned int>(avg + 0.5);
 }
